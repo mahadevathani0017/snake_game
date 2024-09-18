bf9d05c37a8c96bf11651634b1aa2415f8cc5c42
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-09-18 22:46:28 +0530

    create different files for different function.using OOPS

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Turtle, Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,22 +8,10 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
-
+    snake.move()
 screen.exitonclick()
